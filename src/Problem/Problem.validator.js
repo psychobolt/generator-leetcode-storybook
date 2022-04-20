7fc51fcfa2eb13b5c60c0cd335dd17918d9d8810
psychobolt <560721+psychobolt@users.noreply.github.com>
2022-04-20 16:15:44 -0700

    Fix tests

--- src/Problem/Problem.validator.js
+++ src/Problem/Problem.validator.js
@@ -3,17 +3,15 @@ import spawn from 'cross-spawn';
 export function id(input) {
   const done = this.async();
 
-  const invalid = () => ;
-
   if (typeof input === 'number') {
     const result = spawn.sync('leetcode', ['show', input], { encoding: 'utf-8' });
     if (result.output.find(output => output && output.indexOf('Problem not found!') > -1)) {
-      done('Invalid Problem ID')
+      done('Invalid Problem ID');
     } else {
       done(null, true);
     }
     return;
   }
 
-  done('Invalid input')
+  done('Invalid input');
 }
--- src/Problem/__tests__/Problem.validator-test.js
+++ src/Problem/__tests__/Problem.validator-test.js
@@ -3,21 +3,21 @@ import * as validator from '../Problem.validator.js';
 describe('Validate Problem ID', () => {
   it('Valid', () => {
     const validateId = validator.id.bind({
-      async: (message, value) => expect(value).toBeTruthy(),
+      async: () => (message, value) => expect(value).toBeTruthy(),
     });
-    validateId(0);
+    validateId(1);
   });
 
   it('Invalid Input', () => {
     const validateId = validator.id.bind({
-      async: message => expect(message).toBe('Invalid input'),
+      async: () => message => expect(message).toBe('Invalid input'),
     });
     validateId('');
   });
 
   it('Invalid ID', () => {
     const validateId = validator.id.bind({
-      async: message => expect(message).toBe('Invalid Problem ID'),
+      async: () => message => expect(message).toBe('Invalid Problem ID'),
     });
     validateId(1010101010101);
   });
