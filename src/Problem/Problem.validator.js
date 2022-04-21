dddd5ac9f9d04002383454d59b2c317107c3b0a6
psychobolt <560721+psychobolt@users.noreply.github.com>
2022-04-20 21:25:54 -0700

    enable child process stdout in test environment

--- src/Problem/Problem.validator.js
+++ src/Problem/Problem.validator.js
@@ -1,10 +1,17 @@
 import spawn from 'cross-spawn';
 
+const stdout = process.env.NODE_ENV === 'test' ? 'inherit' : 'ignore';
+
+const spawnOpts = {
+  encoding: 'utf-8',
+  stdio: ['ignore', stdout, stdout],
+};
+
 export function id(input) {
   const done = this.async();
 
   if (typeof input === 'number') {
-    const result = spawn.sync('leetcode', ['show', input], { encoding: 'utf-8' });
+    const result = spawn.sync('leetcode', ['show', input], spawnOpts);
     if (result.output.find(output => output && output.indexOf('Problem not found!') > -1)) {
       done('Invalid Problem ID');
     } else {
