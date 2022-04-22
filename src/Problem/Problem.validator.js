169072dbe51647cfc5c374832899e1ae0fe506f1
psychobolt <560721+psychobolt@users.noreply.github.com>
2022-04-22 11:02:51 -0700

    Do not merge stdout

--- src/Problem/Problem.validator.js
+++ src/Problem/Problem.validator.js
@@ -1,17 +1,10 @@
 import spawn from 'cross-spawn';
 
-const stdout = process.env.NODE_ENV === 'test' ? 'inherit' : 'ignore';
-
-const spawnOpts = {
-  encoding: 'utf-8',
-  stdio: ['ignore', stdout, stdout],
-};
-
 export function id(input) {
   const done = this.async();
 
   if (typeof input === 'number') {
-    const result = spawn.sync('leetcode', ['show', input], spawnOpts);
+    const result = spawn.sync('leetcode', ['show', input], { encoding: 'utf-8' });
     if (result.output.find(output => output && output.indexOf('Problem not found!') > -1)) {
       done('Invalid Problem ID');
     } else {
