96bcf5db6944d837aeddff9b5f3251eb115a62e6
psychobolt <560721+psychobolt@users.noreply.github.com>
2022-04-22 15:43:32 -0700

    Mock spawn calls instead

--- src/Problem/__tests__/Problem.validator-test.js
+++ src/Problem/__tests__/Problem.validator-test.js
@@ -1,11 +1,15 @@
+import spawn from 'cross-spawn';
+
 import * as validator from '../Problem.validator.js';
 
 describe('Validate Problem ID', () => {
   it('Valid', () => {
+    const spy = jest.spyOn(spawn, 'sync').mockReturnValue({ output: [''] });
     const validateId = validator.id.bind({
       async: () => (message, value) => expect(value).toBeTruthy(),
     });
     validateId(1);
+    spy.mockRestore();
   });
 
   it('Invalid Input', () => {
@@ -16,9 +20,11 @@ describe('Validate Problem ID', () => {
   });
 
   it('Invalid ID', () => {
+    const spy = jest.spyOn(spawn, 'sync').mockReturnValue({ output: ['Problem not found!'] });
     const validateId = validator.id.bind({
       async: () => message => expect(message).toBe('Invalid Problem ID'),
     });
     validateId(9999);
+    spy.mockRestore();
   });
 });
