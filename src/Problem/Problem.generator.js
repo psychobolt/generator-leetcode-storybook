7df6e98bfa22c83e06b628400e50e1e96038e4c2
psychobolt <560721+psychobolt@users.noreply.github.com>
2022-04-26 14:58:05 -0700

    Reset markdown styles

--- src/Problem/Problem.generator.js
+++ src/Problem/Problem.generator.js
@@ -27,7 +27,7 @@ export default class Problem extends Generator {
     const problem = resolver.problem(id);
     const { alias, source } = resolver.code(id);
     const name = alias.charAt(0).toUpperCase() + alias.slice(1);
-    const titlePath = [...pathParts.map(pathPart => pathPart.replace(/([A-Z]+)*([A-Z][a-z])/g, '$1 $2').trim()), problem.title].join('/');
+    const titlePath = [...pathParts.map(pathPart => pathPart.replace(/([A-Z]+)*([A-Z][a-z])/g, '$1 $2').trim()), `[${id}] ${problem.title}`].join('/');
     const relativePath = `./${path.join(...pathParts, name)}`;
     const destinationPath = path.join(this.destinationRoot(), relativePath);
     const problemPath = `./${name}.md`;
