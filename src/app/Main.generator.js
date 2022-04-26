2d437d764a288fe78e038b51eb84f271a0a5999e
psychobolt <560721+psychobolt@users.noreply.github.com>
2022-04-26 16:08:30 -0700

    Add flow parse comment

--- src/app/Main.generator.js
+++ src/app/Main.generator.js
@@ -3,7 +3,7 @@ import Generator from 'yeoman-generator';
 
 import Problem from '../Problem/index.js';
 
-const require = createRequire(import.meta.url);
+const require = createRequire(import/*:: ("") */.meta.url);
 export default class Main extends Generator {
   initializing() {
     this.composeWith({
